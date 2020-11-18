0008303c78758528b8f986267ee489bfabb4e0aa
MaxZay <54762301+MaxZay@users.noreply.github.com>
2020-11-18 09:18:13 +0300

    В классе Bakery переопределил ToString(), Equals(object obj), GetHAskCode().

---
+++ EpamTask1/Bakery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EpamTask1
+{
+    public class Bakery
+    {
+        List<Production> Products { get; set; }  // Список продукции
+
+        public override string ToString()  // Переопределение ToString()
+        {
+            return string.Join(" ", Products);
+        }
+
+        public override bool Equals(object obj)  // Переопределение Equals()
+        {
+            Bakery bakery = (Bakery)obj;
+            return bakery.Products == this.Products;
+        }
+
+        public override int GetHashCode()  // Переопределение GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 19;
+                foreach (Production product in Products)
+                {
+                    hash = hash * 31 + product.GetHashCode();
+                }
+                return hash;
+            }
+        }
+    }
+}
