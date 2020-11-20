9fdf8118e957172c9159199c102573f6374991a2
MaxZay <54762301+MaxZay@users.noreply.github.com>
2020-11-20 19:42:02 +0300

    Подправил чтение из файла

--- EpamTask1/File.cs
+++ EpamTask1/File.cs
@@ -11,48 +11,56 @@ namespace BakeryLib
     {
         public static Bakery ReadFromFile(string path) // метод для чтения данных из файла
         {
-            Bakery bakery;
-            using (StreamReader sr = new StreamReader(path))
+            
+            try
             {
-                List<Production> products = new List<Production>();
-                List<Ingredient> ingredients = new List<Ingredient>();
-                NumberFormatInfo nfi = new NumberFormatInfo() { NumberDecimalSeparator = "." };
-                Ingredient ingredient;
-                Production product;
-                string line = "";
-                string[] splitedLine;
-                string nameProduct = "", nameIngridient;
-                bool flag = false;
-                int markup = 0;
-                while ((line = sr.ReadLine()) != null)
+                Bakery bakery;
+                using (StreamReader sr = new StreamReader(path))
                 {
-                    if (line[0] != ' ')
+                    List<Production> products = new List<Production>();
+                    List<Ingredient> ingredients = new List<Ingredient>();
+                    NumberFormatInfo nfi = new NumberFormatInfo() { NumberDecimalSeparator = "." };
+                    Ingredient ingredient;
+                    Production product;
+                    string line = "";
+                    string[] splitedLine;
+                    string nameProduct = "", nameIngridient;
+                    bool flag = false;
+                    int markup = 0;
+                    while ((line = sr.ReadLine()) != null)
                     {
-                        if(flag == true)
+                        if (line[0] != ' ')
                         {
-                            product = new Production(nameProduct, ingredients, markup);
-                            flag = false;
-                            products.Add(product);
-                            ingredients = new List<Ingredient>();                        
+                            if (flag == true)
+                            {
+                                product = new Production(nameProduct, ingredients, markup);
+                                flag = false;
+                                products.Add(product);
+                                ingredients = new List<Ingredient>();
+                            }
+                            splitedLine = line.Split(' ');
+                            nameProduct = string.Join(" ", splitedLine.Where(u => Array.IndexOf(splitedLine, u) != splitedLine.Length - 1));
+                            markup = int.Parse(splitedLine[splitedLine.Length - 1]);
                         }
-                        splitedLine = line.Split(' ');
-                        nameProduct = string.Join(" ", splitedLine.Where(u => Array.IndexOf(splitedLine, u) != splitedLine.Length - 1));
-                        markup = int.Parse(splitedLine[splitedLine.Length - 1]);
+                        else
+                        {
+                            splitedLine = line.Trim().Split(' ');
+                            nameIngridient = string.Join(" ", splitedLine.TakeWhile(u => u.ToCharArray().All(char.IsLetter)));
+                            flag = true;
+                            ingredient = new Ingredient(nameIngridient, decimal.Parse(splitedLine[splitedLine.Length - 3], nfi), float.Parse(splitedLine[splitedLine.Length - 2]),
+                                float.Parse(splitedLine[splitedLine.Length - 1]));
+                            ingredients.Add(ingredient);
+                        }
                     }
-                    else
-                    {
-                        splitedLine = line.Trim().Split(' ');
-                        nameIngridient = string.Join(" ", splitedLine.TakeWhile(u => u.ToCharArray().All(char.IsLetter)));
-                        flag = true;
-                        ingredient = new Ingredient(nameIngridient, decimal.Parse(splitedLine[splitedLine.Length - 3], nfi), float.Parse(splitedLine[splitedLine.Length - 2]), 
-                            float.Parse(splitedLine[splitedLine.Length - 1]));
-                        ingredients.Add(ingredient);
-                    }
+                    products.Add(product = new Production(nameProduct, ingredients, markup));
+                    bakery = new Bakery(products.ToArray());
                 }
-                products.Add(product = new Production(nameProduct, ingredients, markup));
-                bakery = new Bakery(products.ToArray());
+                return bakery;
             }
-            return bakery;
+            catch(Exception e)
+            {
+                return null;
+            }
         }
     }
 }
