0ff213ae529b68a94da10f346f881abe87614ce9
MaxZay <54762301+MaxZay@users.noreply.github.com>
2020-11-30 18:35:54 +0300

    Добавил класс одежды

---
+++ ProductsLib/Clothes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductsLib
+{
+    public class Clothes: Product
+    {
+        public string Name { get; set; }
+
+        public Clothes(string productType,
+            decimal purchasePrice,
+            int markup,
+            int quantity, string name) : base(productType,
+            purchasePrice,
+            markup,
+            quantity)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException("Неверное наименование продукта");
+            }
+            Name = name;
+        }
+    }
+}
