ae23508269e45d2bbf0a9137322f1e1fe75a842d
MaxZay <54762301+MaxZay@users.noreply.github.com>
2020-11-30 19:52:26 +0300

    Добавил чтение и запись в файл

---
+++ ProductsLib/WorkWithFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Runtime.Serialization.Json;
+
+namespace ProductsLib
+{
+    public static class WorkWithFile
+    {
+        public static void Save(List<Product> products, string fileName)
+        {
+            var jsonFormatter = new DataContractJsonSerializer(typeof(List<Product>));
+            using (FileStream fs = new FileStream("ProductsInfo.txt", FileMode.OpenOrCreate))
+            {
+                jsonFormatter.WriteObject(fs, products);
+            }
+        }
+
+        public static List<Product> Read(string fileName)
+        {
+            var jsonFormatter = new DataContractJsonSerializer(typeof(List<Product>));
+            using(FileStream fs = new FileStream("ProductsInfo.txt", FileMode.OpenOrCreate))
+            {
+                var newProducts = jsonFormatter.ReadObject(fs) as List<Product>;
+                return (List<Product>)newProducts;
+            }
+            
+        }
+    }
+}
+
+    
